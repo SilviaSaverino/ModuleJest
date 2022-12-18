78c13b9dc2b4e9a3203cc9a1688f5082451cfd92
Silvia Saverino <silviasave89@gmail.com>
2022-12-18 19:53:53 +0000

    jest module - red-green factor explanation

---
+++ scripts/tests/calc.js
@@ -0,0 +1,12 @@
+/* first function with just enough code to pass the first test
+function addition() {
+    return 42;
+}
+
+module.exports = addition;*/
+
+function addition(num1, num2) {
+    return num1 + num2; 
+};
+
+module.exports = addition;
